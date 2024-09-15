da2833b128b39ea84ee2c8fb65a05b9c9e4a1136
randomqwerty <randomqwerty@users.noreply.github.com>
2024-09-15 12:53:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -950,7 +950,7 @@ function updatemap() {
       advantagedDollsNames = mission_info.adaptive_gun.split(",").map((gun_id) => getGunName(gun_id));
     }
     let advantagedDolls = advantagedDollsNames.length > 0 ? advantagedDollsNames.join(", ") : UI_TEXT["mission_info_no_advantaged_dolls"];
-    let levelPenalty = auto_mission_info ? auto_mission_info.expect_gun_level + 10 : null;
+    let levelPenalty = auto_mission_info ? auto_mission_info.expect_gun_level + 10 : 0;
     let baseExperience = mission_info.exp_parameter * 10;
     let tableBody = "";
     let notes = [];
